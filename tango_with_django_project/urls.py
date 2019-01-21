6f5af140478ccba4b06564e50eaef32250e704e2
Simona Paulauskaite <2327031p@student.gla.ac.uk>
2019-01-21 20:30:35 +0000

    migrations and models

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     url('admin/', admin.site.urls),
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path
 from django.conf.urls import url
 from django.conf.urls import include
 from rango import views
